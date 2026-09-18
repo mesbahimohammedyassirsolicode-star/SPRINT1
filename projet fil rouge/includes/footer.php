62d6b08d6e380e1ab2b07b5771aaa34b805fb3f0
mesbahi mohammed yassir <mesbahi.mohammedyassir.solicode@gmail.com>
2026-09-18 16:06:54 +0100

    add

--- projet fil rouge/includes/footer.php
+++ projet fil rouge/includes/footer.php
@@ -1,6 +1,7 @@
 </main>
 </div>
 </div>
+<?php ob_end_flush(); ?>
 <script src="assets/app.js"></script>
 </body>
 </html>
--- projet fil rouge/includes/header.php
+++ projet fil rouge/includes/header.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+ob_start();
+
 require_once __DIR__ . "/../config.php";
 require_once __DIR__ . "/../auth.php";
 require_admin();
